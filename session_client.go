bf4c2e743bb088279d6c16b0c6a1dacb27fc9668
Niels de Vries <niels@queueup.eu>
2020-01-31 15:15:21 +0100

    List sessions of external id

--- session_client.go
+++ session_client.go
@@ -24,6 +24,26 @@ func (s sessionClient) DeleteSession(sessionId string) error {
 	return Dynamo.deleteItem(s.sessionTable, "session_id", sessionId)
 }
 
+func (s sessionClient) ListSessionsOfExternalId(externalId string) (*[]session, error) {
+
+	sessions := &[]session{}
+
+	result, err := Dynamo.listItems(s.sessionTable, "card_number-index", []QueryCondition{
+		{
+			Key:       "card_number",
+			Value:     externalId,
+			Operation: "EQ",
+		},
+	}, sessions, 50)
+
+	if err != nil {
+		return nil, err
+	}
+
+	sessions = result.(*[]session)
+	return sessions, nil
+}
+
 func (s sessionClient) ListExpiredSessions() (*[]session, error) {
 
 	sessions := &[]session{}
